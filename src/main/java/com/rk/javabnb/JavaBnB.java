0458b92d53b28d110093916a97714f3398d0a1bb
KatiaN14 <e.novakova@edu.uah.es>
2024-04-02 09:01:33 +0200

    He actualizado el main

--- src/main/java/com/rk/javabnb/JavaBnB.java
+++ src/main/java/com/rk/javabnb/JavaBnB.java
@@ -11,6 +11,6 @@ package com.rk.javabnb;
 public class JavaBnB {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Roberto!");
     }
 }
