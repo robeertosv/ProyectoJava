a6cccd4f3b35bcaa91603421a6f1f744a9e19d86
robeertosv <=>
2024-04-02 09:04:32 +0200

    Update

--- src/main/java/com/rk/javabnb/JavaBnB.java
+++ src/main/java/com/rk/javabnb/JavaBnB.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.rk.javabnb;
 
-/**
- *
- * @author Roberto
- */
 public class JavaBnB {
 
     public static void main(String[] args) {
