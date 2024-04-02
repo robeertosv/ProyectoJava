82c3653c7e917dc15de6d5507fd1d928774278e0
robeertosv <=>
2024-04-02 08:58:06 +0200

    Create project

---
+++ src/main/java/com/rk/javabnb/JavaBnB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rk.javabnb;
+
+/**
+ *
+ * @author Roberto
+ */
+public class JavaBnB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
