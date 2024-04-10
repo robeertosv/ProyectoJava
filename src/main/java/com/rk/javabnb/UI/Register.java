82d2bb115a1f4c0108d1738dd8be3da70d4073b8
robeertosv <=>
2024-04-10 13:34:57 +0200

    Funciones de registro

--- src/main/java/com/rk/javabnb/UI/Register.java
+++ src/main/java/com/rk/javabnb/UI/Register.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package com.rk.javabnb.UI;
-
-/**
- *
- * @author Roberto
- */
 public class Register extends javax.swing.JFrame {
 
     /**
--- src/main/java/com/rk/javabnb/Usuarios/Cliente.java
+++ src/main/java/com/rk/javabnb/Usuarios/Cliente.java
@@ -2,31 +2,50 @@ package com.rk.javabnb.Usuarios;
 
 import com.rk.javabnb.db.DataHandler;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Cliente implements Serializable {
+public class Cliente extends Component implements Serializable {
     private String clave;
     private String DNI;
     private String email;
     private String nombre;
     private int tfno;
 
+    DataHandler handler = new DataHandler();
+    ArrayList<Cliente> clientes = handler.getClientes();
+
     public Cliente(String clave, String DNI, String email, String nombre, int tfno){
 
     }
 
-    public void registrar(String clave, String DNI, String email, String nombre, int tfno) {
-        if(correos.contains(email)){
-            System.out.println("El email ya existe");
-        }else{ //verificar que el telefono tiene nueve digitos etc...
-            this.clave = clave;
-            this.DNI = DNI;
-            this.email = email;
-            this.nombre = nombre;
-            this.tfno = tfno;
-            dataHandler.actualizarClientes(this);
+    public boolean registrar(String clave, String DNI, String email, String nombre, int tfno) {
+        for(Cliente c : this.clientes) {
+            if(c.email == email){
+                System.out.println("El email ya existe");
+                //El usuario final no tiene acceso a los logs, por lo que se deberá mostrar por consola
+                JOptionPane.showMessageDialog(this, "Ese correo ya está en uso", "Error de registro", JOptionPane.WARNING_MESSAGE);
+                return false;
+            }
+            else{
+                String tfn = ""+tfno;
+                if(tfn.length() != 9) {
+                    JOptionPane.showMessageDialog(this, "El numero de teléfono está mal formateado", "Error", JOptionPane.WARNING_MESSAGE);
+                    return false;
+                } //verificar que el telefono tiene nueve digitos etc...
+
+                this.clave = clave;
+                this.DNI = DNI;
+                this.email = email;
+                this.nombre = nombre;
+                this.tfno = tfno;
+                handler.actualizarClientes(this); //Hacer que esto no de error
+            }
         }
+
+        return true;
     }
 
     public void login(String email, String clave) {
