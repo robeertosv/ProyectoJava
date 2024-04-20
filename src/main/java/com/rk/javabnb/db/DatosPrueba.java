0478b7354d749245c4dc8c654515f982478a3ed2
robeertosv <=>
2024-04-20 15:48:24 +0200

    Update TestFile

--- src/main/java/com/rk/javabnb/db/DatosPrueba.java
+++ src/main/java/com/rk/javabnb/db/DatosPrueba.java
@@ -21,10 +21,12 @@ public class DatosPrueba {
         Inmueble casa1 = new Inmueble("Casa Roberto", 'c', new ArrayList<>(), 1000, new ArrayList<>(), new Direccion("Alcalá" ,"Belvis", 3, 28806), new DatosInmueble(2, 3, 3, 3,"Casa bonita"));
         Inmueble casa2 = new Inmueble("Casa Katia", 'c', new ArrayList<>(), 800, new ArrayList<>(), new Direccion("Madrid" ,"Belvis", 3, 28806), new DatosInmueble(2, 3, 3, 3,"Casa bonita"));
         Inmueble casa3 = new Inmueble("Casa Pepe", 'c', new ArrayList<>(), 900, new ArrayList<>(), new Direccion("Cuenca" ,"Belvis", 3, 28806), new DatosInmueble(2, 3, 3, 3,"Casa bonita"));
+        Inmueble casa4 = new Inmueble("Casa Luis", 'c', new ArrayList<>(), 825, new ArrayList<>(), new Direccion("Madrid", "Bel", 3, 28805), new DatosInmueble(2,2,2,2,"Casa coquette"));
 
         casa1.setCalificacion(2.63);
         casa2.setCalificacion(5);
         casa3.setCalificacion(1.8);
+        casa4.setCalificacion(4.8);
         Database.save();
 
     }
