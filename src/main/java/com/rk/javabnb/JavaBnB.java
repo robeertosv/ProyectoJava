39136ed6ba66b36d997efdb3f32d2a4812833568
Rober <121724164+robeertosv@users.noreply.github.com>
2024-04-29 16:58:20 +0200

    Quitar warnings

--- src/main/java/com/rk/javabnb/JavaBnB.java
+++ src/main/java/com/rk/javabnb/JavaBnB.java
@@ -1,26 +1,18 @@
 package com.rk.javabnb;
 
-
-import com.rk.javabnb.Inmuebles.*;
 import com.rk.javabnb.UI.*;
 import com.rk.javabnb.Usuarios.*;
-import com.rk.javabnb.db.DataChecker;
 import com.rk.javabnb.db.Database;
-import com.rk.javabnb.db.DatosPrueba;
-
-
 import java.io.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
 
 public class JavaBnB implements Serializable {
 
     public static void main(String[] args) {
-        Database db = new Database();//Cargar los datos desde la db
+        new Database();//Cargar los datos desde la db
 
 
-        if(Database.getCurrentUser().size() != 0) {
-            Object user = Database.getCurrentUser().get(0);
+        if(!Database.getCurrentUser().isEmpty()) {
+            Object user = Database.getCurrentUser().getFirst();
             if (user instanceof Admin){
                 new MenuAdmin();
             }else if(user instanceof ClienteParticular){
@@ -31,19 +23,5 @@ public class JavaBnB implements Serializable {
         }else {
             new Login();
         }
-
-        /*
-        Admin admin = new Admin();
-        ArrayList<Object> logeados = new ArrayList<>();
-        logeados.add(admin);
-        Database.setCurrentUser(logeados);
-        new Home("admin");
-
-        /*ArrayList<Admin> admins = new ArrayList<>();
-        admins.add(admin);
-        Database.setCurrentUser(admins);
-        new HomeAdmin("inmuebles");*/
-
-
     }
 }
