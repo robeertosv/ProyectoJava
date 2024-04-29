fe5b3354f5fc06a2d6757bac1000624fa6d7ef5d
robeertosv <=>
2024-04-29 16:19:10 +0200

    Generar factura

--- src/main/java/com/rk/javabnb/Inmuebles/Reserva.java
+++ src/main/java/com/rk/javabnb/Inmuebles/Reserva.java
@@ -5,8 +5,10 @@ import com.rk.javabnb.Usuarios.Cliente;
 import com.rk.javabnb.Usuarios.ClienteParticular;
 import com.rk.javabnb.db.Database;
 
-import java.io.Serializable;
+import javax.swing.*;
+import java.io.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static java.time.temporal.ChronoUnit.DAYS;
@@ -32,6 +34,7 @@ public class Reserva implements Serializable {
         //this.getAnfitrion().addReserva(this);
         Database.addReserva(this);
         Database.save();
+        this.generarFactura();
     }
     
     public LocalDate getEntrada(){return this.fechaEntrada;}
@@ -63,6 +66,54 @@ public class Reserva implements Serializable {
         long dias = DAYS.between(fechaEntrada, fechaSalida);
         double precio = this.inmueble.getPrecio()*dias;
         return precio;
+    }
+
+    public void generarFactura() {
+        try{
+            PrintWriter salida = new PrintWriter(new BufferedWriter(new FileWriter(LocalDate.now()+"_"+LocalDateTime.now().getNano()+"_factura.txt")));
+            salida.println("-------- CONFIRMACIÓN DE RESERVA --------");
+            salida.println("JAVABNB                        "+ LocalDate.now());
+            salida.println("*****************************************");
+            salida.println("Datos del cliente");
+            salida.println();
+            salida.println("Nombre: " + this.cliente.getNombre());
+            salida.println("Correo: " + this.cliente.getEmail());
+            salida.println();
+            salida.println("*****************************************");
+            salida.println("Datos del inmueble");
+            salida.println("Nombre: " + this.inmueble.getTitulo());
+            salida.println("Habitaciones: " + this.inmueble.getDatos().getHabitaciones());
+            salida.println("Camas: " + this.inmueble.getDatos().getCamas());
+            salida.println("Huéspedes reservados: " + this.huespedes);
+            salida.println("Fecha de entrada: " + this.fechaEntrada);
+            salida.println("Fecha de salida: " + this.fechaSalida);
+            salida.println();
+            salida.println("*****************************************");
+            salida.println("Datos bancarios");
+            salida.println();
+            salida.println("Método de pago: Tarjeta");
+            String numeroTarjeta = this.cliente.getTarjetaS();
+            String numero = numeroTarjeta.substring(numeroTarjeta.length()-4);
+            salida.println("Nº: **** **** **** " + numero);
+            salida.println("Titular: " + this.cliente.getTarjeta().getTitular() );
+            salida.println("Subtotal: " + this.calcularPrecio());
+            if(this.cliente.isVIP) {
+                salida.println("Descuentos: " +"Sí");
+                salida.println("TOTAL: " + this.calcularPrecio()*0.9);
+            }else {
+                salida.println("Descuentos: " +"NO");
+                salida.println("TOTAL: " + this.calcularPrecio());
+            }
+            salida.println();
+            salida.println("*****************************************");
+            salida.println("Por favor, conserve este documento hasta\nla finalización de su estancia");
+            salida.println();
+
+            salida.close();
+        }catch(IOException ioe) {
+            System.out.println("Error de IO: "+ioe);
+        }
+
     }
     public String toString(){
         return this.inmueble.toString()+this.cliente.getNombre();
