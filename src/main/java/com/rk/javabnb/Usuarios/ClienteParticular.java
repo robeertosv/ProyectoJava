4208415631a12fd10d6ac9e27de52b41a7acd2be
Rober <121724164+robeertosv@users.noreply.github.com>
2024-05-12 14:36:09 +0200

    Documentar ClienteParticular

--- src/main/java/com/rk/javabnb/Usuarios/ClienteParticular.java
+++ src/main/java/com/rk/javabnb/Usuarios/ClienteParticular.java
@@ -20,28 +20,50 @@ public class ClienteParticular extends Cliente implements Serializable {
         Database.save();
     }
 
+    /**@return la tarjeta del ClienteParticular*/
     public TarjetaCredito getTarjeta() {return tarjeta;}
+
+    /**sirve para mostrar el número de la tarjeta o informar sobre su ausencia en el perfil o preview del cliente
+     * @return el texto a mostrar*/
     public String getTarjetaS(){
         String text = this.tarjeta!=null?this.tarjeta.getNumeroS():"Todavía no ha sido configurado ningún método de pago";
         return text;
         //sirve para mostrar el número de la tarjeta o informar sobre su ausencia en el perfil o preview del cliente
     }
+    /**@return true si es VIP*/
     public boolean isVIP() {return isVIP;}
+
+    /**@return el texto que se debe mostrar si el ClienteParticular es VIP*/
     public String getVIPS(){
         String status = isVIP ? "VIP" : "no VIP";
         return status;
     }
+    /**@param tarjeta es la tarjeta asociada al ClienteParticular*/
     public void setTarjeta(TarjetaCredito tarjeta) {
         this.tarjeta = tarjeta;
         Database.save();
     }
 
+    /**
+     * Sirve para saber que inmuebles ha valorado este ClienteParticular y que de esta manera no pueda valorar varias veces el mismo inmueble
+     * @param i el inmueble que ha valorado
+     * */
     public void addInmuebleValorado(Inmueble i) { this.inmueblesValorados.add(i); Database.save(); }
+
+    /**
+     * @return los inmuebles valorados por este ClienteParticular*/
     public ArrayList<Inmueble> getInmueblesValorados() { return this.inmueblesValorados; }
+
+    /**
+     * Hace que este ClienteParticular sea VIP
+     */
     public void hacerVIP(){
         this.isVIP = true;
         Database.save();
     }
+
+    /**
+     * Hace que este ClienteParticular deje de ser VIP*/
 
     public void noVIP() {
         this.isVIP = false;
